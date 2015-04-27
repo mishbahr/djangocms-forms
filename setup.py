3a0f15e50657c78e909b7eec797e935ff91af047
Mishbah <mishbah@jp74.com>
2015-04-27 20:46:49 +0100

    Update Development Status

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     zip_safe=False,
     keywords='djangocms-forms',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
