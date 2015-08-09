f430423123b8eea3327d645b060bc98ecbdd732b
Mishbah Razzaque <mishbah@jp74.com>
2015-08-10 00:11:02 +0100

    CMS Apphook

---
+++ djangocms_forms/cms_app.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+
+
+class DjangoCMSFormsApphook(CMSApp):
+    name = _('Django CMS Forms')
+    urls = ['djangocms_forms.urls']
+
+apphook_pool.register(DjangoCMSFormsApphook)
