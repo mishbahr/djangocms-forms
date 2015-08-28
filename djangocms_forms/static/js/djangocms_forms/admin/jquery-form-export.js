2a6c63d8ae5e101d38b92eeac09431530391e287
Mishbah Razzaque <mishbah@jp74.com>
2015-08-28 17:22:41 +0100

    Fix #12
    — Fix #12
    — Refactor variable names.

--- djangocms_forms/static/js/djangocms_forms/admin/jquery-form-export.js
+++ djangocms_forms/static/js/djangocms_forms/admin/jquery-form-export.js
@@ -1,41 +1,44 @@
 (function($) {
     $(function() {
-        var formsubmission_form = $('#formsubmission_form');
-        var form_field = $('select[id="id_form"]', formsubmission_form);
-        var headers_field = $('.field-headers', formsubmission_form);
+        var form = $('#formsubmission_form'),
+            formSelectorInput = $('select[id="id_form"]', form),
+            headersFieldWrapper = $('.field-headers', form);
 
-        var loadHeaders = function() {
-            if (!form_field.val()) {
-                headers_field.slideUp('fast');
+        var fetchAvailableHeaders = function() {
+            if (!formSelectorInput.val()) {
+                headersFieldWrapper.slideUp('fast');
                 return;
             }
 
             var data = {
-                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val(),
-                'form': $('select[name="form"]').val(),
-                'file_type': $('select[name="file_type"]').val()
+                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]', form).val(),
+                'form': formSelectorInput.val(),
+                'file_type': $('select[id="id_file_type"]', form).val()
             };
 
             $.ajax({
                 type: 'POST',
-                url: '',
                 data: data,
                 success: function(response) {
                     if (response.reloadBrowser) {
-                        location.reload();
+                        location.reload(true);
                     }
-                    $('select[id="id_headers_to"]').find('option').remove();
+                    var headersInput__from = $('select[id="id_headers_from"]', form),
+                        headersInput__to = $('select[id="id_headers_to"]', form);
 
-                    var headers_input_from = $('select[id="id_headers_from"]');
-                    headers_input_from.find('option').remove();
+                    headersInput__to.find('option').remove();
+                    headersInput__from.find('option').remove();
 
                     $.each(response.availableHeaders, function(index, value) {
-                        headers_input_from.append('<option value=' + value + '>' + value + '</option>');
+                        headersInput__from.append($('<option/>', {
+                            value: value,
+                            text: value
+                        }));
                     });
 
                     SelectBox.init('id_headers_from');
                     SelectBox.init('id_headers_to');
-                    headers_field.slideDown();
+                    headersFieldWrapper.slideDown();
                 },
                 error: function() {
                     alert('We\'re sorry. Something unexpected happened. Please reload the page and try again.');
@@ -46,8 +49,8 @@
         };
 
         if ($('body').hasClass('export-form')) {
-            loadHeaders();
-            form_field.change(loadHeaders);
+            fetchAvailableHeaders();
+            formSelectorInput.change(fetchAvailableHeaders);
         }
     });
 })(django.jQuery);
