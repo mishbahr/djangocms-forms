15a046899d2d6994181a88681419442c11c5df09
Amos Vryhof <amos@vryhofresearch.com>
2019-12-02 11:13:36 -0500

    Recaptcha code from mcldev

--- djangocms_forms/widgets.py
+++ djangocms_forms/widgets.py
@@ -20,13 +20,3 @@ class DateInput(widgets.TextInput):
 
 class TimeInput(widgets.TextInput):
     input_type = 'time'
-
-
-class ReCaptchaWidget(widgets.Widget):
-
-    def render(self, name, value, attrs=None):
-        template = '<div class="g-recaptcha" id="%(widget_id)s"></div>'
-        return mark_safe(template % {'widget_id': 'id_%s' % name})
-
-    def value_from_datadict(self, data, files, name):
-        return (data.get('g-recaptcha-response', None), )
