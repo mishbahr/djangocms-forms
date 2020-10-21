b20bab3203abb08fff6ed730e56c1a414cd2f737
Adrian Pachzelt <GrazingScientist@users.noreply.github.com>
2020-10-21 10:53:05 +0200

    Update models.py

--- djangocms_forms/models.py
+++ djangocms_forms/models.py
@@ -9,7 +9,7 @@ from cms.models.fields import PageField
 from django.db import models
 from django.db.models import DO_NOTHING
 from django.template.defaultfilters import slugify
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from jsonfield import JSONField
 from unidecode import unidecode
